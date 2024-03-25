cdd725a361bba42e5f0d14852ed7f0b1185d2b33
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2024-03-25 00:01:17 -0300

    Implement rudimentary left and right rotations'

--- avl.py
+++ avl.py
@@ -7,6 +7,7 @@ class Node:
     value: int
     left: Optional['Node'] = None
     right: Optional['Node'] = None
+    parent: Optional['Node'] = None
 
     def __str__(self) -> str:
         left_value = self.left.value if self.left else None
@@ -28,28 +29,24 @@ class AVLTree:
         def __insert(node: Node, value: int):
             if value == node.value:
                 return
-            elif value < node.value:
+            if value < node.value:
                 if node.left is None:
                     node.left = Node(value)
+                    node.left.parent = node
                 else:
                     __insert(node.left, value)
             else:
                 if node.right is None:
                     node.right = Node(value)
+                    node.right.parent = node
                 else:
                     __insert(node.right, value)
         
         __insert(self.root, value)
     
     def search(self, value: int) -> Optional[int]:
-        def __search(node: Optional[Node], value: int) -> Optional[int]:
-            if not node:
-                return None
-            elif node.value == value:
-                return value
-            else:
-                return __search(node.left, value) or __search(node.right, value)
-        return __search(self.root, value)    
+        res = AVLTree.__search(self.root, value)
+        return res.value if res else None
     
     def height(self) -> int:
         def __height(node: Optional[Node]) -> int:
@@ -88,4 +85,49 @@ class AVLTree:
             buffer.write('\n')
         
         return buffer.getvalue()
+
+    def rotate(self, value: int, dir: str) -> None:
+        rot = AVLTree.__right_rotate if dir == 'right' else AVLTree.__left_rotate
+        A = AVLTree.__search(self.root, value)
+        if A:
+            parent = A.parent
+            if parent:
+                if parent.value > A.value:
+                    parent.right = rot(A)
+                else:
+                    parent.left = rot(A)
+            else:
+                self.root = rot(A)
+        else:
+            print(f'Value {value} was not found.')
+
+    
+    @staticmethod 
+    def __search(node: Optional[Node], value: int) -> Optional[Node]:
+        if not node:
+            return None
+        elif node.value == value:
+            return node
+        else:
+            return AVLTree.__search(node.left, value) or AVLTree.__search(node.right, value)
+
+    @staticmethod
+    def __right_rotate(A: Node) -> Node:
+        assert(A.left is not None)
+        B = A.left
+        A.left = B.right
+        if B.right: B.parent = A
+        B.right = A
+        A.parent = B
+        return B
+    
+    @staticmethod
+    def __left_rotate(A: Node) -> Node:
+        assert(A.right is not None)
+        B = A.right
+        A.right = B.left
+        if B.left: B.parent = A
+        B.left = A
+        A.parent = B
+        return B
 
--- main.py
+++ main.py
@@ -8,22 +8,34 @@ while True:
     try:
         command = input("Insert a command: ")
         command = command.split(' ')
-        (command, num) = command[0], int(command[1])
+        if len(command) >= 2:
+            (command, num) = command[0], int(command[1])
+        else:
+            (command, num) = command[0], 0
+
     except:
         break
     
     match command:
         case 'i':
-            tree.insert(num)
+            tree.insert(int(num))
             os.system('cls'); print(tree.graph())
         case 's':
-            print(f'Found: {tree.search(num)}\n')
+            print(f'Found: {tree.search(int(num))}\n')
         case 'r':
             randnum = random.randint(10, 99)
             tree.insert(randnum)
             os.system('cls'); print(tree.graph())
             print(f'Tried to insert {randnum}')
+        case 'rr':
+            tree.rotate(num, 'right')
+            os.system('cls'); print(tree.graph())
+        case 'rl':
+            tree.rotate(num, 'left')
+            os.system('cls'); print(tree.graph())   
+        case 'q':
+            break
         case _:
-            print("Incorrect command.\n")
+            print("Fucky command inserted.")
 
     
