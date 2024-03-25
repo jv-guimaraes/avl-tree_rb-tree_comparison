148b56128544948392d3ee5c2db9b0f174c6ca5f
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2024-03-24 22:32:30 -0300

    Add option to insert random number

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import random
 from avl import AVLTree
 
 tree = AVLTree()
+
 while True:
     try:
         command = input("Insert a command: ")
@@ -13,10 +15,14 @@ while True:
     match command:
         case 'i':
             tree.insert(num)
-            os.system('cls')
-            print(tree.graph())
+            os.system('cls'); print(tree.graph())
         case 's':
             print(f'Found: {tree.search(num)}\n')
+        case 'r':
+            randnum = random.randint(10, 99)
+            tree.insert(randnum)
+            os.system('cls'); print(tree.graph())
+            print(f'Tried to insert {randnum}')
         case _:
             print("Incorrect command.\n")
 
